de20029d6cc140b6736da5ea278b587b7893b48a
rithishcodespace <rithishcodespace@gmail.com>
2025-02-15 17:19:18 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ let Header = () =>{
     return(
         <div className="bg-black text-white flex flex-wrap justify-around h-18  shadow-slate-600 shadow-lg">
             <img src="https://tse3.mm.bing.net/th?id=OIP.Vc2uO4I4bGdncIiA5usFOwHaHa&pid=Api&P=0&h=180" className="h-16 w-18 mt-1"/>
-            <Link to="/home"><p className="mt-6 cursor-pointer">HOME</p></Link>
-            <Link to="/home"><p className="mt-6 cursor-pointer">ABOUT</p></Link>
-            <Link to="/home"><p className="mt-6 cursor-pointer">CONTACT</p></Link>
-            <Link to="/home"><p className="mt-6 cursor-pointer">SERVICE</p></Link>
-            <Link to="/home"><p className="mt-6 cursor-pointer">LOGIN</p></Link>
+            <Link to="/"><p className="mt-6 cursor-pointer">HOME</p></Link>
+            <Link to="/about"><p className="mt-6 cursor-pointer">ABOUT</p></Link>
+            <Link to="/contact"><p className="mt-6 cursor-pointer">CONTACT</p></Link>
+            <Link to="/service"><p className="mt-6 cursor-pointer">SERVICE</p></Link>
+            <Link to="/login"><p className="mt-6 cursor-pointer">LOGIN</p></Link>
         </div>
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
 let Login = () =>{
     return(
-        <center>
-            <div>
-                <h1 className="text-black">Login page</h1>
+            <div className="h-auto mx-auto flex items-center justify-center relative top-40">
+                <div className="mx-auto w-96 border-slate-300 border-[2px]">
+                <p className="text-black text-2xl font-semibold text-center mt-4">Login page</p>
+                    <form action="" className="flex flex-col items-center">
+                        <input type="text" className="border-[2px] border-black m-5 h-12 w-72 rounded-md"/><br />
+                        <input type="password" className="border-[2px] border-black mt-5 h-12 w-72 rounded-md"/><br />
+                        <button className="h-12 w-24 bg-black text-white rounded-2xl mb-2">submit</button>
+                    </form>
+                </div>
             </div>
-        </center>
     )
 }
 export default Login;
